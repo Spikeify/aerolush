56da0b67fd019daf3be2c358d0a4a6aeec7c242e
Dean Gostiša <dean@black.si>
2015-12-23 19:51:40 +0100

    Added test with updating existing documents, added default _id field for object primary key

--- src/test/java/com/aerolush/UpdatingIndexTest.java
+++ src/test/java/com/aerolush/UpdatingIndexTest.java
@@ -10,10 +10,10 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,11 +23,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class TermsIndexTest {
+public class UpdatingIndexTest {
 
 	private final Aerospike spike;
 
-	public TermsIndexTest() {
+	public UpdatingIndexTest() {
 
 		spike = new Aerospike();
 	}
@@ -59,8 +59,25 @@ public class TermsIndexTest {
 	}
 
 	@Before
-	public void prepareIndex() throws IOException {
+	public void beforeTest() throws IOException {
 		getSfy().truncateNamespace("test");
+	}
+
+	/**
+	 * storing two things here ... the whole line ... and the line number
+	 */
+	private static void addDoc(IndexWriter w, Media media) throws IOException {
+		Document doc = new Document();
+		doc.add(new StringField("_id", media.id, Field.Store.YES));
+		doc.add(new StringField("channelId", media.channelId, Field.Store.YES));
+		for (String gameId : media.gameIds) {
+			doc.add(new StringField("gameIds", gameId, Field.Store.YES));
+		}
+		w.updateDocument(new Term("_id", media.id), doc);
+	}
+
+	@Test
+	public void testSearch() throws IOException {
 		List<String> gameIds = Arrays.asList("game1", "game2", "game3", "game4", "GAME1");
 		List<String> channelIds = Arrays.asList("ch1", "ch2", "ch3", "ch4", "ch5");
 		Random randomizer = new Random();
@@ -100,30 +117,37 @@ public class TermsIndexTest {
 
 		long timeSpent = System.currentTimeMillis() - startTime;
 		System.out.println("Time spent: " + timeSpent);
-	}
 
-	/**
-	 * storing two things here ... the whole line ... and the line number
-	 */
-	private static void addDoc(IndexWriter w, Media media) throws IOException {
-		Document doc = new Document();
-		doc.add(new StringField("_id", media.id, Field.Store.YES));
-		doc.add(new StringField("channelId", media.channelId, Field.Store.YES));
-		for (String gameId : media.gameIds) {
-			doc.add(new StringField("gameIds", gameId, Field.Store.YES));
+		// reopen index
+		analyzer = new StandardAnalyzer();
+		index = new AeroDirectory(getSfy());
+		config = new IndexWriterConfig(analyzer);
+		w = new IndexWriter(index, config);
+
+		int totalCh1 = find(null, "ch1");
+		int totalGame1 = find("game1", null);
+		int totalGame1AndCh1 = find("game1", "ch1");
+
+		// update index
+		for (Media media : list) {
+			if (media.channelId.equals("ch1") && media.gameIds.contains("game1")) {
+				media.channelId = "ch2";
+				media.gameIds.remove("game1");
+				Document doc = new Document();
+				doc.add(new StringField("channelId", media.channelId, Field.Store.YES));
+				for (String gameId : media.gameIds) {
+					doc.add(new StringField("gameIds", gameId, Field.Store.YES));
+				}
+				w.updateDocument(new Term("_id", media.id), doc);
+			}
 		}
-		w.updateDocument(new Term("_id", media.id), doc);
+		w.close();
+		Assert.assertEquals(totalCh1 - totalGame1AndCh1, find(null, "ch1"));
+		Assert.assertEquals(totalGame1 - totalGame1AndCh1, find("game1", null));
+		Assert.assertEquals(0, find("game1", "ch1"));
 	}
 
-	@Test
-	public void testSearch() throws IOException {
-
-		find(null, "ch1");
-		find("game1", null);
-		find("game1", "ch1");
-	}
-
-	private void find(String gameId, String channelId) throws IOException {
+	private int find(String gameId, String channelId) throws IOException {
 		System.out.println("Game ID: " + gameId + ", Channel ID: " + channelId);
 		BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
 		if (gameId != null) {
@@ -169,6 +193,7 @@ public class TermsIndexTest {
 		// reader can only be closed when there
 		// is no need to access the documents any more.
 		reader.close();
+		return data.totalHits;
 	}
 
 }
